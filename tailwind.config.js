43423717ec42bcbb78597ab10f9db534182b0ccc
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2025-07-16 13:30:52 +0300

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: [
-    './*.html',
-    './*.js',
+  content: ["./*.html", "./*.js"],
+  safelist: [
+    "bg-primary-500",
+    "bg-primary-600",
+    "bg-secondary-500",
+    "bg-secondary-600",
+    "text-white",
+    "rounded-full",
+    "shadow-lg",
+    // add any other classes you use dynamically
   ],
   theme: {
     extend: {},
